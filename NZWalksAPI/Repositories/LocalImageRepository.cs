d4945904b3decaa38f458358ab065c19577687a1
andiradita <inyoman.andiradita@gmail.com>
2024-07-22 12:14:31 +0800

    implement upload image api

---
+++ NZWalksAPI/Repositories/LocalImageRepository.cs
@@ -0,0 +1,43 @@
+﻿using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Repositories
+{
+    public class LocalImageRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly NZWalksDbContext dbContext;
+
+        public LocalImageRepository(IWebHostEnvironment webHostEnvironment,
+            IHttpContextAccessor httpContextAccessor,
+            NZWalksDbContext dbContext)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.httpContextAccessor = httpContextAccessor;
+            this.dbContext = dbContext;
+        }
+        public async Task<Image> Upload(Image image)
+        {
+            var localFilePath = Path.Combine(webHostEnvironment.ContentRootPath,
+                "Images", $"{image.FileName}{image.FileExtention}");
+
+            //upload image to local path
+            using var stream = new FileStream(localFilePath, FileMode.Create);
+            await image.File.CopyToAsync(stream);
+
+            // http://localhost:1000/images/image.jpg
+            var urlFilePath = $"{httpContextAccessor.HttpContext.Request.Scheme}://" +
+                $"{httpContextAccessor.HttpContext.Request.Host}" +
+                $"/Images/" +
+                $"{image.FileName}{image.FileExtention}";
+            image.FilePath = urlFilePath;
+
+            //add image to the images table
+            await dbContext.Images.AddAsync(image);
+            await dbContext.SaveChangesAsync();
+            
+            return image;
+        }
+    }
+}
